2c0b5ccd7652ef11644f515a010141b9492da618
ssssss45 <ssssss45@gmail.com>
2018-08-21 19:20:09 +0300

    -added aStar movement type. these enemies will find player using A* algorythm (code taken from http://buildnewgames.com/astar/) and follow him. currently these can't pass through level border tunnels

--- js/enemies.js
+++ js/enemies.js
@@ -36,6 +36,7 @@ class enemy
 		{
 			case 0: this.move = this.randomMovement;break;
 			case 1: this.move = this.followIfSeen;break;
+			case 2: this.move = this.aStar;break;
 		}
 
 	}
@@ -116,8 +117,6 @@ class enemy
 		}
 		else
 		{
-			console.log(this.lastPlayerX);
-			console.log(this.lastPlayerY);
 			return this.character.move(this.dx, this.dy, this.direction, level);
 		}
 
@@ -170,7 +169,182 @@ class enemy
 		}
 	}
 
+/*
+ █████╗       
+██╔══██╗▄ ██╗▄
+███████║ ████╗
+██╔══██║▀╚██╔▀
+██║  ██║  ╚═╝ 
+╚═╝  ╚═╝                
+*/
+	aStar(level, playerX, playerY)
+	{
+		var path = findPath(level, [this.character.y,this.character.x], [playerY, playerX]);
 
+		var dx = path[1][1] - this.character.x;
+		var dy = path[1][0] - this.character.y;
+		var direction = this.getDirectionFromDxDy(dx,dy);
+
+		return this.character.move(dx, dy, direction, level);
+
+		function findPath(world, pathStart, pathEnd)
+		{
+			var	abs = Math.abs;
+			var	max = Math.max;
+			var	pow = Math.pow;
+			var	sqrt = Math.sqrt;
+
+			var maxWalkableTileNum = 0;
+
+			var worldWidth = world[0].length;
+			var worldHeight = world.length;
+			var worldSize =	worldWidth * worldHeight;
+
+			// which heuristic should we use?
+			// default: no diagonals (Manhattan)
+			var distanceFunction = ManhattanDistance;
+			var findNeighbours = function(){};
+
+			function ManhattanDistance(Point, Goal)
+			{
+				return abs(Point.x - Goal.x) + abs(Point.y - Goal.y);
+			}
+
+			function Neighbours(x, y)
+			{
+				var	N = y - 1;
+				var S = y + 1;
+				var E = x + 1;
+				var W = x - 1;
+
+				if (E < 0) E = worldWidth;
+				if (W > worldWidth) W = 0;
+
+				if (N < 0) N = worldHeight;
+				if (S > worldHeight) S = 0;
+
+				var myN = canWalkHere(x, N),
+				myS = canWalkHere(x, S),
+				myE = canWalkHere(E, y),
+				myW = canWalkHere(W, y),
+				result = [];
+				if(myN)
+				result.push({x:x, y:N});
+				if(myE)
+				result.push({x:E, y:y});
+				if(myS)
+				result.push({x:x, y:S});
+				if(myW)
+				result.push({x:W, y:y});
+				findNeighbours(myN, myS, myE, myW, N, S, E, W, result);
+				return result;
+			}
+
+			function canWalkHere(x, y)
+			{
+				return ((world[x] != null) && (world[x][y] !=1));
+			};
+
+			function Node(Parent, Point)
+			{
+				var newNode = {
+					// pointer to another Node object
+					Parent:Parent,
+					// array index of this Node in the world linear array
+					value:Point.x + (Point.y * worldHeight),
+					// the location coordinates of this Node
+					x:Point.x,
+					y:Point.y,
+					// the distanceFunction cost to get
+					// TO this Node from the START
+					f:0,
+					// the distanceFunction cost to get
+					// from this Node to the GOAL
+					g:0
+				};
+
+				return newNode;
+			}
+
+			function calculatePath()
+			{
+				// create Nodes from the Start and End x,y coordinates
+				var	mypathStart = Node(null, {x:pathStart[0], y:pathStart[1]});
+				var mypathEnd = Node(null, {x:pathEnd[0], y:pathEnd[1]});
+				// create an array that will contain all world cells
+				var AStar = new Array(worldSize);
+				// list of currently open Nodes
+				var Open = [mypathStart];
+				// list of closed Nodes
+				var Closed = [];
+				// list of the final output array
+				var result = [];
+				// reference to a Node (that is nearby)
+				var myNeighbours;
+				// reference to a Node (that we are considering now)
+				var myNode;
+				// reference to a Node (that starts a path in question)
+				var myPath;
+				// temp integer variables used in the calculations
+				var length, max, min, i, j;
+				// iterate through the open list until none are left
+				while(length = Open.length)
+				{
+					max = worldSize;
+					min = -1;
+					for(i = 0; i < length; i++)
+					{
+						if(Open[i].f < max)
+						{
+							max = Open[i].f;
+							min = i;
+						}
+					}
+					// grab the next node and remove it from Open array
+					myNode = Open.splice(min, 1)[0];
+					// is it the destination node?
+					if(myNode.value === mypathEnd.value)
+					{
+						myPath = Closed[Closed.push(myNode) - 1];
+						do
+						{
+							result.push([myPath.x, myPath.y]);
+						}
+						while (myPath = myPath.Parent);
+						// clear the working arrays
+						AStar = Closed = Open = [];
+						// we want to return start to finish
+						result.reverse();
+					}
+					else // not the destination
+					{
+						// find which nearby nodes are walkable
+						myNeighbours = Neighbours(myNode.x, myNode.y);
+						// test each one that hasn't been tried already
+						for(i = 0, j = myNeighbours.length; i < j; i++)
+						{
+							myPath = Node(myNode, myNeighbours[i]);
+							if (!AStar[myPath.value])
+							{
+								// estimated cost of this particular route so far
+								myPath.g = myNode.g + distanceFunction(myNeighbours[i], myNode);
+								// estimated cost of entire guessed route to the destination
+								myPath.f = myPath.g + distanceFunction(myNeighbours[i], mypathEnd);
+								// remember this new path for testing above
+								Open.push(myPath);
+								// mark this node in the world graph as visited
+								AStar[myPath.value] = true;
+							}
+						}
+						// remember this route as having no more untested options
+						Closed.push(myNode);
+					}
+				} // keep iterating until until the Open list is empty
+				return result;
+			}
+			return calculatePath();
+		}
+	}
 /*
 ███╗   ███╗██╗███████╗ ██████╗
 ████╗ ████║██║██╔════╝██╔════╝
@@ -212,6 +386,21 @@ class enemy
 		}
 
 		return result;
+	}
+
+	getDirectionFromDxDy(dx,dy)
+	{
+		switch (dx)
+		{
+			case -1: return 0; break;
+			case 1: return 2; break;
+		}
+
+		switch (dy)
+		{
+			case -1: return 1; break;
+			case 1: return 3; break;
+		}
 	}
 
 	getRandomInt(min, max) 
